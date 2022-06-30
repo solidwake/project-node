46872f471f3bc84ec6c38f9f63e4016328572f53
Idris Westbrook <westbrook.idris@gmail.com>
2022-06-30 13:55:30 -0400

    add filepath and content type

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ path = require('path'),
 fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/') {
+/*     if(req.url === '/') {
         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
             if (err) throw err;
             res.writeHead(200, { 'Content-Type': 'text/html' }); // If the resopnse is 200(OK) then render content in HTML
@@ -37,6 +37,34 @@ const server = http.createServer((req, res) => {
         ];
         res.writeHead(200, { 'Content-Type': 'application/json'});
         res.end(JSON.stringify(users));
+    } */
+
+    // Build the file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+    // Extension of the file
+    let extName = path.extname(filePath);
+
+    // Initial content type
+    let contentType = 'text/html';
+
+    // Check file extension and set content type
+    switch(extName) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
     }
 });
 // Get the PORT from the .env file. If the PORT is not there, use PORT 5000
